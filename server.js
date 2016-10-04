8b04b5fc7e55f81fc4dd9d8f8bfa9a24475169da
eliserothberg <rothberg.elise@gmail.com>
2016-10-03 19:05:55 -0700

    attempting to deploy to heroku

--- server.js
+++ server.js
@@ -1,17 +1,56 @@
+// // var connection = require('../connection.js');
+// var express = require('express');
+// var bodyParser = require('body-parser');
+// var methodOverride = require('method-override')
+// var orm = require('./config/orm.js');
+
+
+// var app = express();
+// //Serve static content for the app from the "public" directory in the application directory.
+// app.use(express.static(__dirname + '/public'));
+
+// // parse application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({
+//     extended: false
+// }))
+// // * `selectAll()` 
+// //     * `insertOne()` 
+// //     * `updateOne()` 
+
+// // orm.select('*', 'burgers');
+// // override with POST having ?_method=DELETE
+// // app.use(methodOverride('_method'))
+// // var exphbs = require('express-handlebars');
+// // app.engine('handlebars', exphbs({
+// //     defaultLayout: 'main'
+// // }));
+
+// // orm.select('*', 'burgers');
+
+//   orm.selectAll('burgers', 'burger_name', 'id', 
+//   function (result) {
+//     console.log(result);
+//   });
+
+//   orm.insertOne('burgers', 'burger_name', 'date', 
+//   function (result) {
+//     console.log(result);
+//   });
+
+//   orm.updateOne('burgers', 'burger_name', 'devoured', 
+//   function (result) {
+//     console.log(result);
+//   });
+
+
+/*
+Here is where you set up your server file.
+express middleware.
+*/
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
-var Sequelize = require('sequelize'),
-  connection;
-  if (process.env.JAWSDB_URL) {
-    connection = new Sequelize(process.env.JAWSDB_URL);
-  } else {
-    connection = new Sequelize('burgers_db', 'root', 'password', {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: '3306'
-    })
-  }
 
 var app = express();
 
