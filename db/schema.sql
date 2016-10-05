f9c66ceb4a09bcbb163884e9e7dbe5e26f5487fb
eliserothberg <rothberg.elise@gmail.com>
2016-10-04 18:24:45 -0700

    updating for JAWSDB

--- db/schema.sql
+++ db/schema.sql
@@ -1,24 +1,10 @@
--- CREATE DATABASE burgers_db;
--- USE burgers_db;
-
--- CREATE TABLE burgers
--- (
---   id INT NOT NULL AUTO_INCREMENT,
---   burger_name VARCHAR(255) NOT NULL,
---   devoured BOOLEAN DEFAULT false,
---     date DATE,
---   PRIMARY KEY (id)
--- );
-
-CREATE DATABASE burgers_db;
-USE burgers_db;
 
 CREATE TABLE burgers
 (
   id INT NOT NULL AUTO_INCREMENT,
   burger_name VARCHAR(255) NOT NULL,
   devoured BOOLEAN DEFAULT false,
-  date TIMESTAMP,
+  date TIMESTAMP NOT NULL,
   PRIMARY KEY (id)
 );
 
--- server.js
+++ server.js
@@ -1,52 +1,3 @@
-// // var connection = require('../connection.js');
-// var express = require('express');
-// var bodyParser = require('body-parser');
-// var methodOverride = require('method-override')
-// var orm = require('./config/orm.js');
-
-
-// var app = express();
-// //Serve static content for the app from the "public" directory in the application directory.
-// app.use(express.static(__dirname + '/public'));
-
-// // parse application/x-www-form-urlencoded
-// app.use(bodyParser.urlencoded({
-//     extended: false
-// }))
-// // * `selectAll()` 
-// //     * `insertOne()` 
-// //     * `updateOne()` 
-
-// // orm.select('*', 'burgers');
-// // override with POST having ?_method=DELETE
-// // app.use(methodOverride('_method'))
-// // var exphbs = require('express-handlebars');
-// // app.engine('handlebars', exphbs({
-// //     defaultLayout: 'main'
-// // }));
-
-// // orm.select('*', 'burgers');
-
-//   orm.selectAll('burgers', 'burger_name', 'id', 
-//   function (result) {
-//     console.log(result);
-//   });
-
-//   orm.insertOne('burgers', 'burger_name', 'date', 
-//   function (result) {
-//     console.log(result);
-//   });
-
-//   orm.updateOne('burgers', 'burger_name', 'devoured', 
-//   function (result) {
-//     console.log(result);
-//   });
-
-
-/*
-Here is where you set up your server file.
-express middleware.
-*/
 
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -74,5 +25,5 @@ var routes = require('./controllers/burgers_controller.js');
 
 app.use('/', routes);
 
-var port = 3000;
+var port = process.env.PORT || 3000;
 app.listen(port);
